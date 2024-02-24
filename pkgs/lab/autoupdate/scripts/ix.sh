745bcf48ac45c5b041af3825b0f2328d4aaa2dc1
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-25 00:57:20 +0300

    better

--- pkgs/lab/autoupdate/scripts/ix.sh
+++ pkgs/lab/autoupdate/scripts/ix.sh
@@ -7,7 +7,7 @@ cat << EOF > autoupdate_cycle
 #!/usr/bin/env sh
 set -xue
 sleep 200
-(cd ix; git pull) || (rm -rf ix; git clone https://github.com/pg83/ix --branch lab --single-branch ix)
+(cd ix; git pull) || (rm -rf ix; git clone https://github.com/pg83/lab ix)
 cd ix
 export IX_ROOT=/ix
 export IX_EXEC_KIND=system
