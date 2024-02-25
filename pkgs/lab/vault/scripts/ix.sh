1c95115a940c495c670be5a49e790118962fc0ba
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-25 04:32:29 +0300

    better

--- pkgs/lab/vault/scripts/ix.sh
+++ pkgs/lab/vault/scripts/ix.sh
@@ -6,15 +6,9 @@ cd ${out}; mkdir bin; cd bin
 cat << EOF > vault_cycle
 #!/usr/bin/env sh
 set -xue
-
 sleep 5
-
-mkdir -p /root/.ssh /home/root/.ssh
-
-chmod 0700 /root/.ssh
+mkdir -p /home/root/.ssh
 chmod 0700 /home/root/.ssh
-
-cp /etc/sudo/authorized_keys /root/.ssh/
 cp /etc/sudo/authorized_keys /home/root/.ssh/
 EOF
 
