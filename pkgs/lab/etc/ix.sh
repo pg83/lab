895cb4117504e3cbe2d552314dcaac705bc5ee98
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-02-25 18:36:02 +0300

    add dns

---
+++ pkgs/lab/etc/ix.sh
@@ -0,0 +1,9 @@
+{% extends '//die/proxy.sh' %}
+
+{% block install %}
+mkdir -p ${our}/etc/hosts.d
+
+cat << EOF > ${out}/etc/hosts.d/01-locals
+10.0.0.85 host1
+EOF
+{% endblock %}
--- pkgs/lab/ix.sh
+++ pkgs/lab/ix.sh
@@ -1,6 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+lab/etc
 lab/vault
 bin/ix/ci
 set/stalix
