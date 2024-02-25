e5a27c5de95abe10f4e29b4963a8b90e97d49e42
Anton Samokhvalov <pg@yandex-team.ru>
2024-02-25 15:40:13 +0300

    better

--- pkgs/lab/vault/ix.sh
+++ pkgs/lab/vault/ix.sh
@@ -1,11 +1,16 @@
 {% extends '//die/hub.sh' %}
 
-{% block pubkey %}
+{% block pubkey1 %}
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZvPrN52SQSe5Xh4lqatDvx0yUftwtY0NZZ5FdAfnYa+QeZRnsxvAHq2RsFb32zl3SHPcuvaaE1yPNzRTuQivf56URCZ82xyIVmd/5DlU/7lZzJHiPnmaVvE2oJFdynOYsQcMZEnoXNpspA0hOx3b7I9Td6Zd/gJrushgZ0j7t28eztWXJaysGCg5kM5bGRNuJTUxk8Ql9Ag+CESJTg/7Ka98RzWyWHc8WLBtCLyfB+885cuiCeULLBUVk2JVIReIi8fs3Lhbwe6GFQfny9hEjJRNQyvUL4ibw0Sh1r7+mXwzo9uryt0AG1tOvo9hVQV8FksRhr6d5MZk3aLDmw7gxXz/Wh0s9DMtsE6ONPATK6NGWrbrO4paU8hClxz80E7uEQ/wM9LPrxspuSGYHrZQapsoJag173MhR9ChixEIXcs4ZVAZKJ9BhyH9e9U1GGUe5qbWgdA6Ve4kxSSvs2l6M1HzvEGrN8mmnktXcNZrTS6+V69Klwtdx7g4wWb/k5vU= pg@stalix
+{% endblock %}
+
+{% block pubkey2 %}
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZvPrN52SQSe5Xh4lqatDvx0yUftwtY0NZZ5FdAfnYa+QeZRnsxvAHq2RsFb32zl3SHPcuvaaE1yPNzRTuQivf56URCZ82xyIVmd/5DlU/7lZzJHiPnmaVvE2oJFdynOYsQcMZEnoXNpspA0hOx3b7I9Td6Zd/gJrushgZ0j7t28eztWXJaysGCg5kM5bGRNuJTUxk8Ql9Ag+CESJTg/7Ka98RzWyWHc8WLBtCLyfB+885cuiCeULLBUVk2JVIReIi8fs3Lhbwe6GFQfny9hEjJRNQyvUL4ibw0Sh1r7+mXwzo9uryt0AG1tOvo9hVQV8FksRhr6d5MZk3aLDmw7gxXz/Wh0s9DMtsE6ONPATK6NGWrbrO4paU8hClxz80E7uEQ/wM9LPrxspuSGYHrZQapsoJag173MhR9ChixEIXcs4ZVAZKJ9BhyH9e9U1GGUe5qbWgdA6Ve4kxSSvs2l6M1HzvEGrN8mmnktXcNZrTS6+V69Klwtdx7g4wWb/k5vU= pg@stalix
 {% endblock %}
 
 {% block run_deps %}
 lab/vault/scripts
-etc/user(hash=x,pubkey={{self.pubkey().strip()}},user=pg,login_shell=/bin/sh,userid=10000)
+etc/user(hash=x,pubkey={{self.pubkey1().strip()}},user=pg,login_shell=/bin/sh,userid=10000)
+etc/user(hash=x,pubkey={{self.pubkey2().strip()}},user=pgx,login_shell=/bin/sh,userid=10001)
 etc/services/runit(srv_dir=vault,srv_command=exec vault_cycle)
 {% endblock %}
