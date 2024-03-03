0cf105e2bb5742ae459a341ffde75d73faeebf98
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-03 14:02:53 +0300

    better

--- pkgs/lab/services/autoupdate/scripts/ix.sh
+++ pkgs/lab/services/autoupdate/scripts/ix.sh
@@ -7,7 +7,7 @@ cat << EOF > autoupdate_cycle
 #!/usr/bin/env sh
 set -xue
 sleep 100
-(cd ix; git pull) || (rm -rf ix; git clone https://github.com/pg83/lab ix)
+(cd ix; git pull; git submodule update --init --recursive) || (rm -rf ix; git clone --recurse-submodules https://github.com/pg83/lab ix)
 cd ix
 export IX_ROOT=/ix
 export IX_EXEC_KIND=system
