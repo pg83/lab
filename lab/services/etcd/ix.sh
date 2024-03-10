3486ed90786cbab48c2cb37dc437e12b7e096119
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-10 14:08:17 +0300

    better

--- lab/services/etcd/ix.sh
+++ lab/services/etcd/ix.sh
@@ -1,18 +1,8 @@
 {% extends '//die/hub.sh' %}
 
-{% block etcd_conf %}
---name {{hostname}}
---initial-advertise-peer-urls http://10.0.1.10:2380
---listen-peer-urls http://10.0.1.10:2380
---listen-client-urls http://10.0.1.10:2379,http://127.0.0.1:2379
---advertise-client-urls http://10.0.1.10:2379
---initial-cluster-token etcd-cluster-1
---initial-cluster infra0=http://10.0.1.10:2380,infra1=http://10.0.1.11:2380,infra2=http://10.0.1.12:2380 \
---initial-cluster-state new
-{% endblock %}
-
 {% block run_deps %}
 bin/etcd/server
+lab/services/etcd/scripts
 etc/user/nologin(userid=1002,user=etcd)
 etc/services/runit(srv_dir=etcd,srv_user=etcd,srv_command=exec etcd_runner)
 {% endblock %}
---
+++ lab/services/etcd/scripts/ix.sh
@@ -0,0 +1,43 @@
+{% extends '//die/proxy.sh' %}
+
+{% block cluster_map %}
+{{cluster_map | b64d}}
+{% endblock %}
+
+{% block my_ip %}
+{% for x in self.cluster_map() | jl %}
+{% if x["hostname"] == hostname %}
+{{x["ip"]}}
+{% endif %}
+{% endfor %}
+{% endblock %}
+
+{% set my_ip %}{{self.my_ip().strip()}}{% endset %}
+
+{% block all_etcd %}
+{% for x in self.cluster_map() | jl %}
+{{x["hostname"]}}=http://{{x["ip"]}}:2380
+{% endfor %}
+{% endblock %}
+
+{% block etcd_conf %}
+--name {{hostname}}
+--initial-advertise-peer-urls http://{{my_ip}}:2380
+--listen-peer-urls http://{{my_ip}}:2380
+--listen-client-urls http://{{my_ip}}:2379,http://127.0.0.1:2379
+--advertise-client-urls http://{{my_ip}}:2379
+--initial-cluster-token etcd-cluster-1
+--initial-cluster {{','.join(ix.parse_list(self.all_etcd()))}}
+--initial-cluster-state new
+{% endblock %}
+
+{% block install %}
+mkdir ${out}/bin
+
+cat << EOF >> ${out}/bin/etcd_runner
+#!/usr/bin/env sh
+exec etcd {{ix.fix_list(self.etcd_conf())}}
+EOF
+
+chmod +x ${out}/bin/*
+{% endblock %}
