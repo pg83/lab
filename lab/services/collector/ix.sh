7d6d827aed2c1359c7ee1f3acb57aecd33e972b5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-10 21:31:35 +0300

    persist it

--- lab/services/collector/ix.sh
+++ lab/services/collector/ix.sh
@@ -1,10 +1,11 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+bin/su/exec
 bin/prometheus
 bin/prometheus/node/exporter
 lab/services/collector/scripts
 etc/user/nologin(userid=1001,user=collector)
-etc/services/runit(srv_dir=collector,srv_user=collector,srv_command=exec collectd)
+etc/services/runit(srv_dir=collector,srv_command=exec collectd)
 etc/services/runit(srv_dir=node_exporter,srv_user=collector,srv_command=exec node_exporter)
 {% endblock %}
--- lab/services/collector/scripts/ix.sh
+++ lab/services/collector/scripts/ix.sh
@@ -6,8 +6,10 @@ mkdir ${out}/bin ${out}/etc
 cat << EOF > ${out}/bin/collectd
 #!/usr/bin/env sh
 set -xue
+mkdir -p /home/collector
+chown collector:collector /home/collector
 cd /ix/realm/system/share
-exec prometheus --config.file=/etc/prometheus.conf --storage.tsdb.path=/var/run/collector/
+exec su-exec collector prometheus --config.file=/etc/prometheus.conf --storage.tsdb.path=/home/collector/
 EOF
 
 cat << EOF > ${out}/etc/prometheus.conf
