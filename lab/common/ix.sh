ce1110ceb6bf37e074c4d570624a196e2410548b
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-11 20:50:12 +0300

    better

--- lab/common/ix.sh
+++ lab/common/ix.sh
@@ -1,15 +1,17 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-bin/mc
 lab/etc
-set/stalix
-bin/kernel/6/7
-bin/kernel/6/6
 lab/services/etcd
-bin/dropbear/runit
-bin/kernel/gengrub
 lab/services/collector
 lab/services/node/exporter
 lab/services/autoupdate(user=ix)
+
+bin/mc
+bin/kernel/6/7
+bin/kernel/6/6
+bin/kernel/gengrub
+bin/dropbear/runit
+
+set/stalix/server
 {% endblock %}
