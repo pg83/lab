557569d4c4529d73a91b7b830e4fac1cbd1bd3e5
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-13 16:04:55 +0300

    fix

--- lab/services/sftp/runit/ix.sh
+++ lab/services/sftp/runit/ix.sh
@@ -1,8 +1,14 @@
 {% extends '//etc/services/runit/script/ix.sh' %}
 
-{% block srv_command %}
-export SFTPGO_SFTPD__HOST_KEYS=/etc/keys/ssh_rsa,/etc/keys/ssh_ecdsa,/etc/keys/ssh_ed25519
+{% block srv_user_prepare %}
+{{super()}}
+cp /etc/keys/ssh_rsa /var/run/{{srv_dir}}/id_rsa
+cp /etc/keys/ssh_ecdsa /var/run/{{srv_dir}}/id_ecdsa
+cp /etc/keys/ssh_ed25519 /var/run/{{srv_dir}}/id_ed25519
+chown {{srv_user}} /var/run/{{srv_dir}}/id_*
+{% endblock %}
 
+{% block srv_command %}
 exec sftpgo portable \
     --directory {{sftp_dir}} \
     --ftpd-port 8001 \
