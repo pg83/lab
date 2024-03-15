668ecb05f63a1cc58e0488c409220d83f299edad
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-15 15:59:40 +0300

    better

--- lab/services/proxy/runit/ix.sh
+++ lab/services/proxy/runit/ix.sh
@@ -5,15 +5,16 @@ set -xue
 export ETCDCTL_ENDPOINTS=localhost:2379
 export IFACE=$(ip -o addr show | grep 10.0.0 | head -n1 | awk '{print $2}')
 ip addr del 10.0.0.32/24 dev ${IFACE} || true
-exec etcdctl lock proxy -- /bin/sh -c 'echo {{self.us_command() | b64e}} | base64 -d | /bin/sh'
+exec etcdctl lock proxy -- /bin/bash -c 'source <(echo {{self.us_command() | b64e}} | base64 -d)'
 {% endblock %}
 
 {% block us_command %}
 set -xue
 ip addr add 10.0.0.32/24 dev ${IFACE}
-cat << EOF | su -s /bin/sh proxy
+exec su -s /bin/sh proxy <(cat << EOF
 exec reproxy --listen=10.0.0.32:8080 --static.enabled
 EOF
+)
 {% endblock %}
 
 {% block srv_command %}
