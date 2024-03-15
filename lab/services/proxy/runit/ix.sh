6bbf75d18a558794b5edd042a18600076c047791
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-15 15:03:00 +0300

    better

--- lab/services/proxy/runit/ix.sh
+++ lab/services/proxy/runit/ix.sh
@@ -12,9 +12,8 @@ etcdctl lock proxy -- /bin/sh -c 'echo {{self.us_command() | b64e}} | base64 -d 
 set -xue
 ip addr add 10.0.0.32/24 dev ${IFACE}
 cat << EOF | su -s /bin/sh proxy
-reproxy --listen=10.0.0.32:8080 --static.enabled
+exec reproxy --listen=10.0.0.32:8080 --static.enabled
 EOF
-ip addr del 10.0.0.32/24 dev ${IFACE}
 {% endblock %}
 
 {% block srv_command %}
