51616faebb45ff8ddf670fd79e1183bc61c24742
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-16 22:00:45 +0300

    fix

--- lab/hosts/lab2/ix.sh
+++ lab/hosts/lab2/ix.sh
@@ -3,6 +3,6 @@
 {% set cm = cluster_map | des %}
 
 {% block run_deps %}
-lab/services/rsyncd/sync(user=mirror,rsync_share=rsynk://lab3:{{cm.ports.mirror_rsyncd}},rsync_where=/home/mirror/data,delay=100)
+lab/services/rsyncd/sync(user=mirror,rsync_share=rsync://lab3:{{cm.ports.mirror_rsyncd}},rsync_where=/home/mirror/data,delay=100)
 lab/services/ci(ci_targets=set/ci/tier/1)
 {% endblock %}
