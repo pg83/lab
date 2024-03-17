e91f57f42453c2a99e4ab57f1d08b0cae76c99ea
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-18 01:09:28 +0300

    better

--- lab/services/autoupdate/scripts/ix.sh
+++ lab/services/autoupdate/scripts/ix.sh
@@ -11,8 +11,7 @@ EOF
 cat << EOF > autoupdate_cycle
 #!/usr/bin/env sh
 set -xue
-sleep 100
-(cd ix; git pull; git submodule update --init --recursive) || (rm -rf ix; git clone --recurse-submodules https://github.com/pg83/lab ix)
+gclone 100s /var/run/evlog_git_lab/events https://github.com/pg83/lab ix
 cd ix
 export IX_ROOT=/ix
 export IX_EXEC_KIND=system
