2195411721e52ff12de37b3c26ebd7accc7e71d2
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-18 23:38:05 +0300

    better

---
+++ lab/services/git/evque/runit/ix.sh
@@ -0,0 +1,14 @@
+{% extends '//etc/services/runit/script/ix.sh' %}
+
+{% set cm = cluster_map | des %}
+
+{% block srv_command %}
+set -xue
+
+export ETCDCTL_ENDPOINTS=localhost:{{cm.etcd.ports.client}}
+
+gosmee client --saveDir \${PWD} --noReplay "{{evlog_url}}" qw 2>&1 | gnugrep --line-buffered 'has been saved' | while read l; do
+    cat *.json | etcdctl put /git/logs/{{evlog_user}}
+    rm *.json
+done
+{% endblock %}
