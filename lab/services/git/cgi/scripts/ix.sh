f76c75928b80f1bc87ec40c6deabac7bedaeaad4
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-21 18:15:23 +0300

    better

--- lab/services/git/cgi/scripts/ix.sh
+++ lab/services/git/cgi/scripts/ix.sh
@@ -5,7 +5,7 @@ mkdir -p ${out}/etc/hooks
 
 cat << EOF > ${out}/etc/hooks/{{evlog_topic}}.sh
 #!/usr/bin/env sh
-etcdctl put /git/logs/{{evlog_topic}}
+(date; cat) | etcdctl put /git/logs/{{evlog_topic}}
 EOF
 
 chmod +x ${out}/etc/hooks/*
