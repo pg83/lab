5f5df94cfbd45b1f4ce2717d1f6e7d7f7e96f585
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-21 18:26:36 +0300

    better

--- lab/services/git/cgi/scripts/ix.sh
+++ lab/services/git/cgi/scripts/ix.sh
@@ -5,10 +5,10 @@ mkdir -p ${out}/etc/hooks
 
 cat << EOF > ${out}/etc/hooks/{{evlog_topic}}.sh
 #!/usr/bin/env sh
-((date; cat) | etcdctl put /git/logs/{{evlog_topic}}) 1>&2
-echo 'Content-Type: text/plain'
-echo
-echo 'Hello, world!'
+#((date; cat) | etcdctl put /git/logs/{{evlog_topic}}) 1>&2
+echo "Content-Type: text/plain"
+echo ""
+echo "Hello, world!"
 EOF
 
 chmod +x ${out}/etc/hooks/*
