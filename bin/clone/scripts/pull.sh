114892e4497ee47b68d1761f92782e3235b73816
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-03-24 17:09:39 +0300

    fix

--- bin/clone/scripts/pull.sh
+++ bin/clone/scripts/pull.sh
@@ -2,4 +2,4 @@
 
 set -xue
 
-(cd ${2}; git pull; git submodule update --init --recursive) || (rm -rf ${4}; git clone --recurse-submodules "${1}" ${2})
+(cd ${2}; git pull; git submodule update --init --recursive) || (rm -rf ${2}; git clone --recurse-submodules "${1}" ${2})
