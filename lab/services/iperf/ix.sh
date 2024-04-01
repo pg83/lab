e0a219ce5426e999f759ea0cb1211ea15fa1620d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-02 00:51:25 +0300

    fix

--- lab/services/iperf/ix.sh
+++ lab/services/iperf/ix.sh
@@ -5,5 +5,5 @@
 {% block run_deps %}
 bin/iperf
 etc/user/nobody
-etc/services/runit(srv_dir=iperf,srv_command exec iperf -s -p {{cm.ports.iperf}},srv_user=nobody)
+etc/services/runit(srv_dir=iperf,srv_command=exec iperf -s -p {{cm.ports.iperf}},srv_user=nobody)
 {% endblock %}
