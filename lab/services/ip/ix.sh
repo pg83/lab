53c278774305577981a6e6fb364c64fb4424be07
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-03 23:25:42 +0300

    better

--- lab/services/ip/ix.sh
+++ lab/services/ip/ix.sh
@@ -7,5 +7,6 @@ cat << EOF > ${out}/etc/runit/1.d/20-{{ip_iface}}.sh
 ip addr add {{ip_addr}} dev {{ip_iface}}
 route add default gw {{ip_gw}} {{ip_iface}}
 ip link set {{ip_iface}} up
+resolvconf -u
 EOF
 {% endblock %}
