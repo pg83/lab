52a3ceae6c202e5e7dd1d87d42abfca0fdb67a5d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-03 23:23:42 +0300

    better

--- lab/hosts/lab1/ix.sh
+++ lab/hosts/lab1/ix.sh
@@ -1,11 +1,10 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-lab/services/ip(ip_addr=10.0.0.64/24,ip_iface=eth0)
-lab/services/ip(ip_addr=10.0.0.65/24,ip_iface=eth1)
-lab/services/ip(ip_addr=10.0.0.66/24,ip_iface=eth2)
-lab/services/ip(ip_addr=10.0.0.67/24,ip_iface=eth3)
-
+lab/services/ip(ip_addr=10.0.0.64/24,ip_iface=eth0,ip_gw=10.0.0.1)
+lab/services/ip(ip_addr=10.0.0.65/24,ip_iface=eth1,ip_gw=10.0.0.1)
+lab/services/ip(ip_addr=10.0.0.66/24,ip_iface=eth2,ip_gw=10.0.0.1)
+lab/services/ip(ip_addr=10.0.0.67/24,ip_iface=eth3,ip_gw=10.0.0.1)
 lab/services/mirror(mirror_rsync=1)
 lab/services/ci(ci_targets=set/ci)
 {% endblock %}
--- lab/services/ip/ix.sh
+++ lab/services/ip/ix.sh
@@ -5,5 +5,7 @@ mkdir -p ${out}/etc/runit/1.d
 
 cat << EOF > ${out}/etc/runit/1.d/20-{{ip_iface}}.sh
 ip addr add {{ip_addr}} dev {{ip_iface}}
+route add default gw {{ip_gw}} {{ip_iface}}
+ip link set {{ip_iface}} up
 EOF
 {% endblock %}
