b31746629092eaae90824b162d5c43e92aa35ba4
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-06 23:12:47 +0300

    better

--- lab/etc/hosts/ix.sh
+++ lab/etc/hosts/ix.sh
@@ -1,11 +1,16 @@
 {% extends '//die/proxy.sh' %}
 
+{% set cm = cluster_map | des %}
+
 {% block install %}
 mkdir -p ${out}/etc/hosts.d
 
 cat << EOF > ${out}/etc/hosts.d/01-locals
-{% for x in (cluster_map | des).hosts %}
+{% for x in cm.hosts %}
 {{x.ip}} {{x.hostname}}
+{% for h in x.net %}
+{{h.ip}} {{h.if}}.{{x.hostname}}
+{% endfor %}
 {% endfor %}
 EOF
 {% endblock %}
