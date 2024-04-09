9dd7c49b3a48f7b1e404c75e064749ea414cd7cf
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-10 01:52:32 +0300

    better

--- lab/services/serve/ix.sh
+++ lab/services/serve/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
-lab/services/serve/bin
+bin/http/serve
 lab/etc/user(user={{serve_user}})
 etc/services/runit(srv_dir={{'serve_' + serve_port}},srv_user={{serve_user}},srv_command=exec http_serve {{serve_port}} {{serve_from}})
 {% endblock %}
