af6d8175ab432313c6bf8872603c5c93a9459c65
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-13 21:34:09 +0300

    better

--- lab/common/ix.sh
+++ lab/common/ix.sh
@@ -9,10 +9,6 @@
 
 lab/etc
 
-{% for net in hm.net %}
-lab/services/ip(ip_addr={{net.ip}}/{{net.nm}},ip_gw={{net.gw}},ip_iface={{net.if}})
-{% endfor %}
-
 {% if hostname in cm.etcd.hosts %}
 lab/services/etcd
 lab/services/proxy(proxy_ip=10.0.0.32,proxy_port={{cm.ports.proxy_http}})
@@ -29,7 +25,6 @@ bin/gengrub
 bin/kernel/6/8
 bin/kernel/6/7
 bin/dropbear/runit
-
 bin/fixits(delay=10)
 
 set/fs
--- lab/etc/ix.sh
+++ lab/etc/ix.sh
@@ -1,10 +1,18 @@
 {% extends '//die/hub.sh' %}
 
+{% set cm = cluster_map | des %}
+{% set hm = cm.by_host[hostname] %}
+
 {% block run_deps %}
 lab/etc/env
 lab/etc/keys
 lab/etc/hosts
-{% for d in (cluster_map | des).by_host[hostname].get('disabled', []) %}
+
+{% for d in hm.disabled %}
 lab/etc/stopper(srv_dir={{d}})
+{% endfor %}
+
+{% for net in hm.net %}
+lab/etc/ip(ip_addr={{net.ip}}/{{net.nm}},ip_gw={{net.gw}},ip_iface={{net.if}})
 {% endfor %}
 {% endblock %}
