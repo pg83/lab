108f3af81be0b9f4b0e23b8c7ef417bc2470564c
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-14 22:05:41 +0300

    better

--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -1,11 +1,12 @@
 {% extends '//lab/etc/mount/ix.sh' %}
 
 {% block mount %}
+set -x
 btrfs device scan
 mount /dev/sda /home
-mkdir /var/mnt/minio/1
-mkdir /var/mnt/minio/2
-mkdir /var/mnt/minio/3
+mkdir -p /var/mnt/minio/1
+mkdir -p /var/mnt/minio/2
+mkdir -p /var/mnt/minio/3
 mount /dev/sdb1 /var/mnt/minio/1
 mount /dev/sdb2 /var/mnt/minio/2
 mount /dev/sdc1 /var/mnt/minio/3
