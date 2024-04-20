072f5145b1f9062441577a18f1ce1c52ed271654
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-20 12:03:09 +0300

    better

--- lab/hosts/lab3/mount/ix.sh
+++ lab/hosts/lab3/mount/ix.sh
@@ -1,11 +1,12 @@
 {% extends '//lab/etc/mount/ix.sh' %}
 
 {% block mount %}
-rm -rf /home
-mkdir /home
-mount /dev/sda /home
+mkdir -p /var/mnt/home
 mkdir -p /var/mnt/minio/1
 mkdir -p /var/mnt/minio/2
+mount /dev/sda /var/mnt/home
 mount /dev/sdb /var/mnt/minio/1
 mount /dev/sdc /var/mnt/minio/2
+rm -rf /home
+ln -s /var/mnt/home /home
 {% endblock %}
