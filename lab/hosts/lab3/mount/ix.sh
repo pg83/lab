494bba4d73da8488ceda1beefeb56ae0439fc47e
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-20 13:19:47 +0300

    better

--- lab/hosts/lab3/mount/ix.sh
+++ lab/hosts/lab3/mount/ix.sh
@@ -4,9 +4,9 @@
 mkdir -p /var/mnt/home
 mkdir -p /var/mnt/minio/1
 mkdir -p /var/mnt/minio/2
-mount /dev/sda /var/mnt/home
-mount /dev/sdb /var/mnt/minio/1
-mount /dev/sdc /var/mnt/minio/2
+mount LABEL=HOME /var/mnt/home
+mount LABEL=MINIO_1 /var/mnt/minio/1
+mount LABEL=MINIO_2 /var/mnt/minio/2
 rm -rf /home
 ln -s /var/mnt/home /home
 {% endblock %}
