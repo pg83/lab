5242db8d401eef0c2b478cff46880e06ba6d4595
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-25 00:28:09 +0300

    up

--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -1,15 +1,10 @@
 {% extends '//lab/etc/mount/ix.sh' %}
 
 {% block mount %}
-#mkdir -p /var/mnt/home
 mkdir -p /var/mnt/minio/1
 mkdir -p /var/mnt/minio/2
 mkdir -p /var/mnt/minio/3
-mount LABEL=HOME /var/mnt/home
 mount LABEL=MINIO_1 /var/mnt/minio/1
 mount LABEL=MINIO_2 /var/mnt/minio/2
 mount LABEL=MINIO_3 /var/mnt/minio/3
-mkdir -p /home
-#rm -rf /home
-#ln -s /var/mnt/home /home
 {% endblock %}
