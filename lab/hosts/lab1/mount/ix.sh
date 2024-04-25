b70247f61fb90e72053d1f3d1eb0d4f100ece13d
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-25 19:09:01 +0300

    better

--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -11,7 +11,9 @@ mdadm --assemble md0 /dev/sdb2 /dev/sdc2
 mdadm --assemble md1 /dev/md/md0 /dev/sdd2
 echo '/dev/sda' > /sys/fs/bcache/register
 echo '/dev/md/md1' > /sys/fs/bcache/register
+dmesg
 bcache attach /dev/sda /dev/md/md1
+dmesg
 sleep 10
 mkdir -p /var/mnt/ci
 mount /dev/bcache0 /var/mnt/ci
