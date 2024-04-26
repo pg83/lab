ad307b993dfb117e40c3c6241d9274670cfd8e58
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-26 11:52:50 +0300

    better

--- lab/ix.sh
+++ lab/ix.sh
@@ -5,5 +5,5 @@
 {% endblock %}
 
 {% block run_deps %}
-lab/map(cluster_map={{self.cluster_gen() | eval(self.cluster_gen()) | ser}})
+lab/map(cluster_map={{self.cluster_gen() | eval(self.cluster_gen()) | ser}},dev_mngr=fs)
 {% endblock %}
