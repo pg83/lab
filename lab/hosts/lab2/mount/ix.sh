1eb320b8088eb5ae79e9381cc3f8cd1329ac1fa6
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-30 14:48:02 +0300

    better

--- lab/hosts/lab2/mount/ix.sh
+++ lab/hosts/lab2/mount/ix.sh
@@ -2,7 +2,11 @@
 
 {% block mount %}
 mkdir -p /var/mnt/minio/1
+mkdir -p /var/mnt/minio/2
+mkdir -p /var/mnt/minio/3
 mount LABEL=MINIO_1 /var/mnt/minio/1
+mount LABEL=MINIO_2 /var/mnt/minio/2
+mount LABEL=MINIO_3 /var/mnt/minio/3
 btrfs device scan /dev/sda /dev/sdb /dev/sdc
 sleep 10
 btrfs device scan /dev/sda /dev/sdb /dev/sdc
