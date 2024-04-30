5d56c49c675f19cad26a51a3fb49ff06ee3273c9
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-04-30 23:57:59 +0300

    better

--- lab/etc/hosts/ix.sh
+++ lab/etc/hosts/ix.sh
@@ -1,11 +1,13 @@
 {% extends '//die/proxy.sh' %}
 
 {% set cm = cluster_map | des %}
+{% set hm = cm.by_host[hostname] %}
 
 {% block install %}
 mkdir -p ${out}/etc/hosts.d
 
 cat << EOF > ${out}/etc/hosts.d/01-locals
+{{hm.net[1].ip}} minio
 {% for x in cm.hosts %}
 {{x.ip}} {{x.hostname}}
 {{x.nebula.ip}} {{x.hostname}}.nebula
