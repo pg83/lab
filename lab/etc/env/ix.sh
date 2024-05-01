54d7d461c589750f44c3e2dad878cb0f94e42332
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-01 20:06:10 +0300

    better

--- lab/etc/env/ix.sh
+++ lab/etc/env/ix.sh
@@ -5,6 +5,6 @@ mkdir -p ${out}/etc/env.d
 
 cat << EOF > ${out}/etc/env.d/100-etcd
 export ETCDCTL_ENDPOINTS="lab1:2379,lab2:2379,lab3:2379"
-export MC_HOST_minio=https://qwerty:qwerty123@{{hostname}}.eth1:8012
+export MC_HOST_minio=http://qwerty:qwerty123@{{hostname}}.eth1:8012
 EOF
 {% endblock %}
