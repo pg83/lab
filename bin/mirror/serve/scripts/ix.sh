698509640222add9cccdf87f3d02f77da88d89a0
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-05-02 17:13:29 +0300

    better

--- bin/mirror/serve/scripts/ix.sh
+++ bin/mirror/serve/scripts/ix.sh
@@ -6,6 +6,8 @@ cat << EOF > ${out}/etc/hooks/cas.sh
 #!/usr/bin/env sh
 echo "Content-Type: application/stream"
 echo ""
+export HOME=${TMPDIR}/mctmp
+mkdir -p ${HOME}
 # TODO(pg): validate input
 minio-client cat "minio/cas/\${QUERY_STRING}"
 EOF
