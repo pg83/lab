bc1a7b9a68391d3f8b5029cc05930939b8570fc8
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-06-04 23:38:59 +0300

    better

--- lab/hosts/lab3/ix.sh
+++ lab/hosts/lab3/ix.sh
@@ -2,5 +2,6 @@
 
 {% block run_deps %}
 lab/hosts/lab3/mount
-lab/services/ci(ci_targets=set/ci/tier/1,wd=/var/mnt/ci/ci)
+lab/services/ci(ci_targets=set/ci,wd=/var/mnt/ci/ci)
+#lab/services/ci(ci_targets=set/ci/tier/1,wd=/var/mnt/ci/ci)
 {% endblock %}
