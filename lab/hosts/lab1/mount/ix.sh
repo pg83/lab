496c9de7216cd47a477e103b6d0fca28451ac446
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-08 14:46:12 +0300

    better

--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -5,11 +5,14 @@ mkdir ${out}/bin
 
 cat << EOF > ${out}/bin/mount_ci
 set -xue
-mdadm --assemble md0 /dev/sdb2 /dev/sdc2
-mdadm --assemble md1 /dev/md/md0 /dev/sdd2
+devlink
+mdadm --assemble md0 /dev/link/6ce93586-ad55-4248-bb21-d23eac83dbff /dev/link/7965901e-abed-4628-92f2-a1a4194e6d1c
+devlink
+mdadm --assemble md1 /dev/md/md0 /dev/link/40a63ab3-62f3-43dd-843f-fbed27622bf2
 echo '/dev/sda' > /sys/fs/bcache/register
 echo '/dev/md/md1' > /sys/fs/bcache/register
 bcache attach /dev/sda /dev/md/md1
+devlink
 mkdir -p \${1}
 mount /dev/bcache0 \${1}
 chown ci:ci \${1}
