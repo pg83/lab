9e4fbdf6a4ce50e903f06f2ef7b36d8970d3f423
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-09 22:31:35 +0300

    enable

--- lab/hosts/lab1/ix.sh
+++ lab/hosts/lab1/ix.sh
@@ -2,6 +2,6 @@
 
 {% block run_deps %}
 bin/devlink
-#lab/hosts/lab1/mount
-#lab/services/ci(ci_targets=set/ci,wd=/var/mnt/ci/ci)
+lab/hosts/lab1/mount
+lab/services/ci(ci_targets=set/ci,wd=/var/mnt/ci/ci)
 {% endblock %}
--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -3,34 +3,14 @@
 {% block install %}
 mkdir ${out}/bin
 
-cat << EOF > ${out}/bin/mount_ci_old
-#!/usr/bin/env sh
-set -xue
-devlink
-mdadm --assemble md0 /dev/links/6ce93586-ad55-4248-bb21-d23eac83dbff /dev/links/7965901e-abed-4628-92f2-a1a4194e6d1c
-devlink
-mdadm --assemble md1 /dev/md/md0 /dev/links/40a63ab3-62f3-43dd-843f-fbed27622bf2
-echo '/dev/sda' > /sys/fs/bcache/register
-echo '/dev/md/md1' > /sys/fs/bcache/register
-bcache attach /dev/sda /dev/md/md1 || true
-sleep 1
-bcache attach /dev/sda /dev/md/md1 || true
-sleep 1
-bcache attach /dev/sda /dev/md/md1 || true
-sleep 1
-bcache attach /dev/sda /dev/md/md1 || true
-sleep 1
-devlink
-mkdir -p \${1}
-mount /dev/bcache0 \${1}
-chown ci:ci \${1}
-EOF
-
 cat << EOF > ${out}/bin/mount_ci
 #!/usr/bin/env sh
 set -xue
 mkdir -p \${1}
-mount /dev/sda \${1}
+devlink
+mount -t bcachefs /dev/sda:/dev/links/40a63ab3-62f3-43dd-843f-fbed27622
+bf2:/dev/links/6ce93586-ad55-4248-bb21-d23eac83dbff:/dev/links/7965901e-abe
+d-4628-92f2-a1a4194e6d1c \${1}
 mkdir -p \${1}/ci
 chown ci:ci \${1}/ci
 EOF
