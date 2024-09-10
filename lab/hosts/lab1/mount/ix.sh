020d84f145435b53e46189222f2120b1dc9b6706
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-10 11:10:29 +0300

    fix

--- lab/hosts/lab1/mount/ix.sh
+++ lab/hosts/lab1/mount/ix.sh
@@ -8,9 +8,7 @@ cat << EOF > ${out}/bin/mount_ci
 set -xue
 mkdir -p \${1}
 devlink
-mount -t bcachefs /dev/sda:/dev/links/40a63ab3-62f3-43dd-843f-fbed27622
-bf2:/dev/links/6ce93586-ad55-4248-bb21-d23eac83dbff:/dev/links/7965901e-abe
-d-4628-92f2-a1a4194e6d1c \${1}
+mount -t bcachefs /dev/sda:/dev/links/40a63ab3-62f3-43dd-843f-fbed27622bf2:/dev/links/6ce93586-ad55-4248-bb21-d23eac83dbff:/dev/links/7965901e-abed-4628-92f2-a1a4194e6d1c \${1}
 mkdir -p \${1}/ci
 chown ci:ci \${1}/ci
 EOF
