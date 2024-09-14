79737694bdabd9d0c7e8c20a3c46db41ac5d4452
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-14 13:20:47 +0300

    better

--- lab/services/persist/ix.sh
+++
@@ -1,7 +0,0 @@
-{% extends '//etc/services/runit/script/ix.sh' %}
-
-{% block srv_user_prepare %}
-{{super()}}
-mkdir -p /home/{{srv_user}}
-chown {{srv_user}}:{{srv_user}} /home/{{srv_user}}
-{% endblock %}
