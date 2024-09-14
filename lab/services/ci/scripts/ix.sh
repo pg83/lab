b9c43a40423cf7cfb38ea57b1634808a6d3b033f
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-09-14 14:07:05 +0300

    better

--- lab/services/ci/scripts/ix.sh
+++ lab/services/ci/scripts/ix.sh
@@ -12,12 +12,14 @@ export PATH=/bin
 export IX_ROOT={{wd}}/ix_root
 export IX_EXEC_KIND=local
 
-etcdctl watch --prefix /git/logs/git_ci | gnugrep --line-buffered 'PUT' | head -n 1
+sleep 10
 
 gpull https://github.com/pg83/ix ix
 cd ix
-mv \${IX_ROOT}/build/* \${IX_ROOT}/trash/ || true
-exec ./ix build bld/all {{ci_targets}}
+rm -rf \${IX_ROOT}/build \${IX_ROOT}/trash
+./ix build bld/all {{ci_targets}}
+
+etcdctl watch --prefix /git/logs/git_ci | gnugrep --line-buffered 'PUT' | head -n 1
 EOF
 
 chmod +x ${out}/bin/*
