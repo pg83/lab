b2572528ac315ca904d3d057f0583a4678e1ea89
Anton Samokhvalov <anton@samokhvalov.xyz>
2024-11-03 14:16:13 +0300

    better

--- lab/etc/certs/ix.sh
+++ lab/etc/certs/ix.sh
@@ -2,6 +2,7 @@
 
 {% block install %}
 {{super()}}
+find ${out}
 mkdir -p ${out}/etc/ssl
-cp ${out}/share/ssl/ca-bundle.pem ${out}/etc/ssl/
+cp ${out}/share/ssl/c* ${out}/etc/ssl/cert.pem
 {% endblock %}
