092e991fac4c572eec6e6d764a9872eca36a4bb6
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-03-20 11:39:24 +0300

    try

--- lab/etc/env/ix.sh
+++ lab/etc/env/ix.sh
@@ -4,7 +4,7 @@
 mkdir -p ${out}/etc/env.d
 
 cat << EOF > ${out}/etc/env.d/100-etcd
-export ETCDCTL_ENDPOINTS="lab1:2379,lab2:2379,lab3:2379"
+export ETCDCTL_ENDPOINTS="lab1.nebula:8020,lab2.nebula:8020,lab3.nebula:8020"
 export MC_HOST_minio=http://qwerty:qwerty123@{{hostname}}.eth1:8012
 EOF
 {% endblock %}
