58c4502ef5ce5c926c1a4a4ae866159b4214707c
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-23 19:20:22 +0300

    prepare bin list

--- lab/services/ci/scripts/ix.sh
+++ lab/services/ci/scripts/ix.sh
@@ -19,7 +19,9 @@ gpull https://github.com/pg83/ix ix
 cd ix
 
 ./ix build bld/all
-./ix build {{ci_targets}} --jail=1 --seed=1 --tmpfs=1
+./ix mut ci {{ci_targets}} --jail=1 --seed=1 --tmpfs=1
+
+ls -la {{wd}}/ix_root/realm/ci/bin/ > ../binlist
 
 timeout 60s etcdctl watch --prefix /git/logs/git_ci | gnugrep --line-buffered 'PUT' | head -n 1
 EOF
