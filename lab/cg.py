201ae7253996664af6444124bf964a416f842f96
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-05-31 11:36:10 +0300

    fix

--- lab/cg.py
+++ lab/cg.py
@@ -755,6 +755,7 @@ def it_nebula_reals(lh, h, port):
 
 class EtcdPrivate:
     def __init__(self, peers, port_peer, port_client, hostname, etcid, addr, user_name):
+        self.v = 1
         self.etcid = etcid
         self.peers = peers
         self.port_peer = port_peer
