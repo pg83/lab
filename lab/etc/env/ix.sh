2fe10670ea82769caf8c109fbe71ec997db975ab
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-10-08 00:50:55 +0300

    fix

--- lab/etc/env/ix.sh
+++ lab/etc/env/ix.sh
@@ -1,9 +1,9 @@
 {% extends '//die/gen.sh' %}
 
 {% block install %}
-mkdir -p ${out}/etc/env.d
+mkdir -p ${out}/etc/profile.d
 
-cat << EOF > ${out}/etc/env.d/100-etcd
+cat << EOF > ${out}/etc/profile.d/100-etcd
 export ETCDCTL_ENDPOINTS="lab1.nebula:8020,lab2.nebula:8020,lab3.nebula:8020"
 export MC_HOST_minio=http://qwerty:qwerty123@{{hostname}}.eth1:8012
 EOF
--- lab/hosts/lab4/env/ix.sh
+++ lab/hosts/lab4/env/ix.sh
@@ -1,8 +1,8 @@
 {% extends '//die/gen.sh' %}
 
 {% block install %}
-mkdir -p ${out}/etc/env.d
-cat << EOF > ${out}/etc/env.d/101-minio
+mkdir -p ${out}/etc/profile.d
+cat << EOF > ${out}/etc/profile.d/101-minio
 export MC_HOST_minio=http://qwerty:qwerty123@lab3.eth1:8012
 EOF
 {% endblock %}
