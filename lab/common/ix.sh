f3db3d6feedf514fbe05c420a3f129dfde0a41d2
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-02-04 23:56:26 +0300

    better

--- lab/common/ix.sh
+++ lab/common/ix.sh
@@ -10,11 +10,12 @@ bin/mc
 bin/htop
 bin/ix/tmpfs
 etc/host/keys
+bin/lm/sensors
 bin/kernel/6/14
 bin/kernel/6/16
+bin/smart/mon/tools
 bin/fixits(delay=10)
 bin/kernel/gengrub(kernel_boot_flags=rootdelay=20)
-
 set/fs
 set/stalix/server(fetcher_socks5_proxy=localhost:8015)
 {% endblock %}
