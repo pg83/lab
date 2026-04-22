b5ef4666d2ee78197eef6460f7049ad3a2a7aa0d
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-04-23 00:41:01 +0300

    style

--- lab/etc/multihome/ix.sh
+++ lab/etc/multihome/ix.sh
@@ -25,8 +25,8 @@ mkdir -p ${out}/etc/runit/1.d
 
 cat << 'EOF' > ${out}/etc/runit/1.d/30-multihome.sh
 IP=/ix/realm/ip/bin/ip
-{% for net in hm.net if net.if != 'eth0' %}
 
+{% for net in hm.net if net.if != 'eth0' %}
 $IP route replace 10.0.0.0/24 dev {{net.if}} src {{net.ip}} table {{1012 + loop.index}}
 $IP rule add uidrange {{1012 + loop.index}}-{{1012 + loop.index}} lookup {{1012 + loop.index}} priority 1000
 $IP rule add from {{net.ip}} lookup {{1012 + loop.index}} priority 1001
