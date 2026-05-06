8ce40dc07233f48c7d482861a3fa95380e84f6e1
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-05-06 21:36:44 +0300

    bin/gorn, bin/molot: bump shadow packages to 26 / 24
    Mirrors ix-side bump. gorn 26 adds /v1/tasks/<guid>/content/<name> for
    serving any artifact; molot 24's web UI uses it to expose result.json /
    result.zstd / retry-N.json instead of just stdout/stderr.
    
    gorn URL switched to tag form, matching molot's shape.

--- bin/gorn/ix.sh
+++ bin/gorn/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/25
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/gorn/archive/63b1b0b705ca993e35edc3f4fed641aacf4761a4.tar.gz
+https://github.com/pg83/gorn/archive/refs/tags/26.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-a07346b912bee99542130ea26f1bfecef0e2769408c1fcb697c3ef1d8efdae24
+0975c2371d9a80bcd7fd87b3f8a77a3d035c2625967143e564ac217f2de13f71
 {% endblock %}
 
 {% block go_bins %}
--- bin/molot/ix.sh
+++ bin/molot/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/25
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/molot/archive/refs/tags/23.tar.gz
+https://github.com/pg83/molot/archive/refs/tags/24.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-ae5b16c3b3715f4e340ceb358185a019903c2e36bcf7323b6c262a8aab9e8a08
+e3e294b4a58bb7e81fa1eb561426f3c93df3dc87ab51f634a82792b2fda290f6
 {% endblock %}
 
 {% block go_bins %}
