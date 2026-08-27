32941987e25e81a0f34671e4e747699008865296
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-08-27 19:51:49 +0000

    Revert "bound IX updater candidate builds"
    This reverts commit acb7ff781317b35c3124cd31518cf61567ad043f.

--- bin/updater/scripts/updater.py
+++ bin/updater/scripts/updater.py
@@ -26,7 +26,6 @@ import json
 import os
 import re
 import secrets
-import signal
 import ssl
 import subprocess
 import sys
@@ -49,8 +48,6 @@ MC_ALIAS = 'updater_cache'
 GOOD_SHA_CHARS = frozenset('0123456789abcdef')
 BUILD_FLAGS = ('--opengl=fake', '--vulkan=fake', '--seed=1')
 REPOLOGY_PAGE_SIZE = 200
-DEFAULT_BUILD_TIMEOUT_S = 30 * 60
-BUILD_STOP_TIMEOUT_S = 10
 
 GO_LATEST = 26
 GO_TOOL = (
@@ -412,62 +409,24 @@ class MolotBuilder:
         self.env['AWS_SECRET_ACCESS_KEY'] = base_env['AWS_SECRET_ACCESS_KEY_MOLOT']
         self.env['S3_BUCKET'] = 'molot'
         self.env['MOLOT_CACHE'] = str(Path(cache_path).resolve())
-        self.timeout = int(base_env.get(
-            'IX_UPDATER_BUILD_TIMEOUT_S',
-            DEFAULT_BUILD_TIMEOUT_S,
-        ))
-
-        if self.timeout <= 0:
-            raise InfrastructureFailure('IX_UPDATER_BUILD_TIMEOUT_S must be positive')
 
     def build(self, packages):
         cmd = ('./ix', 'build', *BUILD_FLAGS, *packages)
         log('run', *cmd, '(molot)')
 
-        proc = subprocess.Popen(
+        res = subprocess.run(
             cmd,
             cwd=self.repo,
             env=self.env,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             start_new_session=True,
+            check=False,
         )
 
-        try:
-            output, _ = proc.communicate(timeout=self.timeout)
-            returncode = proc.returncode
-        except subprocess.TimeoutExpired:
-            log(f'build timed out after {self.timeout}s; stop process group {proc.pid}')
-
-            try:
-                os.killpg(proc.pid, signal.SIGTERM)
-            except ProcessLookupError:
-                pass
-
-            try:
-                output, _ = proc.communicate(timeout=BUILD_STOP_TIMEOUT_S)
-            except subprocess.TimeoutExpired:
-                try:
-                    os.killpg(proc.pid, signal.SIGKILL)
-                except ProcessLookupError:
-                    pass
-
-                output, _ = proc.communicate()
-
-            output += f'\nupdater: build timed out after {self.timeout}s\n'.encode()
-            returncode = 124
-        except BaseException:
-            try:
-                os.killpg(proc.pid, signal.SIGKILL)
-            except ProcessLookupError:
-                pass
-
-            proc.wait()
-            raise
-
-        sys.stderr.buffer.write(output)
+        sys.stderr.buffer.write(res.stdout)
         sys.stderr.flush()
-        return BuildResult(returncode, output)
+        return BuildResult(res.returncode, res.stdout)
 
 
 class PackageUpdater:
