e5728ea37ce406e4000fbe6b940dd290c01ec68c
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-08-28 13:21:29 +0000

    bin/molot: set vendor archive hash

--- bin/molot/ix.sh
+++ bin/molot/ix.sh
@@ -9,7 +9,7 @@ https://github.com/pg83/molot/archive/refs/tags/27.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-0000000000000000000000000000000000000000000000000000000000000000
+ba116773f442f058fb05bf8d0845ea664379470402302ac637cbcee0e83a9a5a
 {% endblock %}
 
 {% block go_bins %}
