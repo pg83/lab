92deb0d2b438a18318017f18337fbaf6272bbfae
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-09-03 01:02:17 +0000

    disable reverse path filtering

--- etc/arp/ix.sh
+++ etc/arp/ix.sh
@@ -1,6 +1,6 @@
 {% extends '//die/gen.sh' %}
 
-{# Deterministic ARP per-NIC and loose RPF for same-subnet multipath. #}
+{# Deterministic ARP per-NIC; disable reverse-path filtering for multipath. #}
 
 {% block install %}
 mkdir -p ${out}/etc/sysctl.d
@@ -10,7 +10,7 @@ net.ipv4.conf.all.arp_ignore = 1
 net.ipv4.conf.all.arp_announce = 2
 net.ipv4.conf.default.arp_ignore = 1
 net.ipv4.conf.default.arp_announce = 2
-net.ipv4.conf.all.rp_filter = 2
-net.ipv4.conf.default.rp_filter = 2
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.conf.default.rp_filter = 0
 EOF
 {% endblock %}
