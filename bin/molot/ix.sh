28a8dc2328374f8f0eadfedf85f53ea7030e3d4a
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-09-19 17:27:51 +0000

    Expire Molot task records after one hour and deploy Molot 40

--- bin/molot/ix.sh
+++ bin/molot/ix.sh
@@ -5,11 +5,11 @@ bin/go/lang/25
 {% endblock %}
 
 {% block go_url %}
-https://github.com/pg83/molot/archive/refs/tags/39.tar.gz
+https://github.com/pg83/molot/archive/refs/tags/40.tar.gz
 {% endblock %}
 
 {% block go_sha %}
-da723971829966d7684ca2dd5528b9b25d61ddadcf2963da93fee664afe7b2f9
+5558c10102cb9b07a75e41a9863ec4a0aed7f6ad19c11d0cee8f55ca17ee52dc
 {% endblock %}
 
 {% block go_bins %}
